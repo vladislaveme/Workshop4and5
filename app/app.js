d1cc8db9ac0dd0f81e7307168a4e71deeeba4d3c
vladislaveme <vemelyanov@umass.edu>
2016-10-23 22:32:41 +0100

    fb1

---
+++ app/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class UghWorld extends React.Component {
+    render() {
+      return ( < p > ugh, world < /p>);
+      }
+    }
+    ReactDOM.render( < UghWorld / > ,
+      document.getElementById('fb-feed')
+    );
